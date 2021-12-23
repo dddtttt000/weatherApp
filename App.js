892cd490e9e1002e11c4296f6eedae2317c85c7f
songha <dddtttt000@gmail.com>
2021-12-23 20:46:11 +0900

    docs : Add components and api requsts

--- App.js
+++ App.js
@@ -1,38 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native'
+import * as Location from 'expo-location'
 
 // 스크린의 width 를 가져와서 해당 크기만큼 보여준다.
-const {  width: SCREEN_WIDTH } = Dimensions.get("window")
-
+const { width: SCREEN_WIDTH } = Dimensions.get('window')
 
 export default function App() {
+  const [city, setCity] = useState('Loading...')
+  const [location, setLocation] = useState(null)
+  const [ok, setOk] = useState(true)
+
+  const ask = async () => {
+    // grant 를 받아온다.
+    const { granted } = await Location.requestForegroundPermissionsAsync()
+
+    if (!granted) {
+      setOk(false)
+    }
+
+    // 현재 위치를 latitude 위도, longitude 경도를 받아온다.
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false },
+    )
+    setCity(location[0].city)
+  }
+
+  useEffect(() => {
+    ask()
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <ScrollView 
-        horizontal 
+      <ScrollView
+        horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}>
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
-           <Text style={styles.temp}>27</Text>
-           <Text style={styles.desc}>Sunny</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
         </View>
         <View style={styles.day}>
-           <Text style={styles.temp}>27</Text>
-           <Text style={styles.desc}>Sunny</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
         </View>
         <View style={styles.day}>
-           <Text style={styles.temp}>27</Text>
-           <Text style={styles.desc}>Sunny</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.desc}>Sunny</Text>
         </View>
       </ScrollView>
       {/* 상태바를 밝은 색으로 지정 */}
       <StatusBar style="light" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -40,31 +69,31 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'navy',
   },
-  city : {
-    flex : 1.2, 
+  city: {
+    flex: 1.2,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  cityName : {
-    fontSize : 68,
-    fontWeight : "500",
-    color: 'white'
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500',
+    color: 'white',
   },
-  weather : {
+  weather: {
     // backgroundColor : "teal"
   },
   day: {
-    width : SCREEN_WIDTH,
-    alignItems : "center",
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
   },
-  temp : {
-    color : "#fff9",
-    marginTop : 50,
-    fontSize : 178,
+  temp: {
+    color: '#fff9',
+    marginTop: 50,
+    fontSize: 178,
   },
-  desc : {
-    color : "#fff9",
-    marginTop : -30,
-    fontSize : 60,
-  }
-});
+  desc: {
+    color: '#fff9',
+    marginTop: -30,
+    fontSize: 60,
+  },
+})
