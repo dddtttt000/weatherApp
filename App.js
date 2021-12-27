4c16db8cdfb7d32acc2b716a1d1169fe501f2c5d
songha <dddtttt000@gmail.com>
2021-12-27 16:55:27 +0900

    fix : Fix semicolons

--- App.js
+++ App.js
@@ -1,40 +1,42 @@
-import { StatusBar } from 'expo-status-bar'
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native'
-import * as Location from 'expo-location'
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, ScrollView, Dimensions } from "react-native";
+import * as Location from "expo-location";
 
 // 스크린의 width 를 가져와서 해당 크기만큼 보여준다.
-const { width: SCREEN_WIDTH } = Dimensions.get('window')
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const API_KEY = "50dce4045ebe8554cc6612b714499fa1";
 
 export default function App() {
-  const [city, setCity] = useState('Loading...')
-  const [location, setLocation] = useState(null)
-  const [ok, setOk] = useState(true)
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState(null);
+  const [ok, setOk] = useState(true);
 
   const ask = async () => {
     // API로 권한을 요청한다. -> grant 를 받는다.
-    const { granted } = await Location.requestForegroundPermissionsAsync()
+    const { granted } = await Location.requestForegroundPermissionsAsync();
 
     if (!granted) {
-      setOk(false)
+      setOk(false);
     }
 
     // 현재 위치를 latitude 위도, longitude 경도를 받아온다. -> coords 좌표
     const {
       coords: { latitude, longitude },
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
     // 위도와 경도 위치를 가지고 주소를 가져옴 - 디테일한 주소가 나옴!
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
       { useGoogleMaps: false },
-    )
-    setCity(location[0].city)
-  }
+    );
+    setCity(location[0].city);
+  };
 
   useEffect(() => {
-    ask()
-  }, [])
+    ask();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -63,39 +65,39 @@ export default function App() {
       {/* 상태바를 밝은 색으로 지정 */}
       <StatusBar style="light" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'navy',
+    backgroundColor: "navy",
   },
   city: {
     flex: 1.2,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   cityName: {
     fontSize: 68,
-    fontWeight: '500',
-    color: 'white',
+    fontWeight: "500",
+    color: "white",
   },
   weather: {
     // backgroundColor : "teal"
   },
   day: {
     width: SCREEN_WIDTH,
-    alignItems: 'center',
+    alignItems: "center",
   },
   temp: {
-    color: '#fff9',
+    color: "#fff9",
     marginTop: 50,
     fontSize: 178,
   },
   desc: {
-    color: '#fff9',
+    color: "#fff9",
     marginTop: -30,
     fontSize: 60,
   },
-})
+});
